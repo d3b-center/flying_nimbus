83f22841d4b198f3260e0057093b4dfff5bc3afa
Charles Haynes <haynescd@chop.edu>
2026-01-26 15:54:37 -0500

    Update RDS to populate detailed info

--- internal/providers/aws/backend/rds.go
+++ internal/providers/aws/backend/rds.go
@@ -10,30 +10,32 @@ import (
 )
 
 type RDSInstance struct {
-	Id       string
-	Hostname string
-	Port     int
-	Status   string
-	DbEngine string
+	Id                   string
+	Endpoint             string
+	Port                 int32
+	Status               string
+	DbEngine             string
+	DbVersion            string
+	VpcID                string
+	SubnetIds            []string
+	InstanceClass        string
+	VCPU                 int
+	MemoryGiB            int
+	AllocatedStorage     int32
+	IsPubliclyAccessible bool
 }
 
 func (i RDSInstance) Title() string { return i.Id }
 
 func (i RDSInstance) Description() string {
-	return fmt.Sprintf("Status %s Engine %s", i.Status, i.DbEngine)
+	status := fmt.Sprintf("🔴 %s", i.Status)
+
+	if i.Status == "available" {
+		status = fmt.Sprintf("🟢 %s", i.Status)
+	}
+	return status
 }
 func (i RDSInstance) FilterValue() string { return i.Id }
-
-type InstanceConfig struct {
-	InstanceClass string
-	VCPU          int
-	MemoryGiB     int
-}
-
-type NetworkConfig struct {
-	VpcID     string
-	SubnetIds []string
-}
 
 type rdsAPI interface {
 	DescribeDBInstances(ctx context.Context, params *rds.DescribeDBInstancesInput, opts ...func(*rds.Options)) (*rds.DescribeDBInstancesOutput, error)
@@ -64,12 +66,18 @@ func (s *RdsService) ListDBInstances(ctx context.Context) ([]RDSInstance, error)
 
 	for i, db := range rawDBInstances.DBInstances {
 		slog.Debug(fmt.Sprintf("DB return %s", *db.DBInstanceIdentifier))
+
 		instance := RDSInstance{
-			Id:       *db.DBInstanceIdentifier,
-			Hostname: *db.Endpoint.Address,
-			Port:     int(*db.Endpoint.Port),
-			Status:   *db.DBInstanceStatus,
-			DbEngine: *db.Engine,
+			Id:                   *db.DBInstanceIdentifier,
+			Endpoint:             *db.Endpoint.Address,
+			Port:                 (*db.Endpoint.Port),
+			Status:               *db.DBInstanceStatus,
+			DbEngine:             *db.Engine,
+			DbVersion:            *db.EngineVersion,
+			InstanceClass:        *db.DBInstanceClass,
+			VpcID:                *db.DBSubnetGroup.VpcId,
+			IsPubliclyAccessible: *db.PubliclyAccessible,
+			AllocatedStorage:     *db.AllocatedStorage,
 		}
 		slog.Debug(fmt.Sprintf("DBs %v", instance))
 		instances[i] = instance
--- internal/providers/aws/views/rds.go
+++ internal/providers/aws/views/rds.go
@@ -15,7 +15,15 @@ import (
 )
 
 var (
-	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
+	spinnerStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("69")).Align(lipgloss.Center)
+	instancesListStyle = lipgloss.NewStyle().
+				Border(lipgloss.NormalBorder()).
+				BorderForeground(lipgloss.Color("240")).
+				Padding(0, 1)
+	instanceDetailStyle = lipgloss.NewStyle().
+				Border(lipgloss.NormalBorder()).
+				BorderForeground(lipgloss.Color("240")).
+				Padding(0, 1)
 )
 
 type RdsViewModel struct {
@@ -33,9 +41,13 @@ func InitRdsViewModel(appService *app.App) RdsViewModel {
 	items := []list.Item{}
 
 	l := list.New(items, list.NewDefaultDelegate(), 0, 0)
-	l.Title = "Rds"
-	h, v := constants.DocStyle.GetFrameSize()
-	l.SetSize(constants.WindowSize.Width-h, constants.WindowSize.Height-v)
+	l.Title = "Instances"
+	l.SetShowTitle(true)
+	l.SetShowStatusBar(false)
+	l.SetShowHelp(false)
+
+	//h, v := constants.DocStyle.GetFrameSize()
+	//l.SetSize((constants.WindowSize.Width-h)/3, constants.WindowSize.Height-v)
 
 	loader := spinner.New()
 	loader.Style = spinnerStyle
@@ -44,6 +56,7 @@ func InitRdsViewModel(appService *app.App) RdsViewModel {
 	return RdsViewModel{
 		app:       appService,
 		loader:    loader,
+		list:      l,
 		isLoading: true,
 	}
 }
@@ -64,19 +77,42 @@ func (m RdsViewModel) View() string {
 	if m.isLoading {
 		return constants.DocStyle.Render(m.loader.View() + "\n")
 	}
-	return constants.DocStyle.Render(m.list.View() + "\n")
+
+	w, h := constants.DocStyle.GetFrameSize()
+	contentWidth := constants.WindowSize.Width - w
+	contentHeight := constants.WindowSize.Height - h
+
+	instanceListWidth := contentWidth / 3
+	detailsWidth := contentWidth - instanceListWidth
+
+	m.list.SetSize(instanceListWidth, contentHeight)
+
+	left := instancesListStyle.Width(instanceListWidth).Height(contentHeight).Render(m.list.View())
+	right := instanceDetailStyle.Width(detailsWidth).Height(contentHeight).Render(generateInstanceDetail(m.list.SelectedItem()))
+
+	return lipgloss.JoinHorizontal(lipgloss.Top, left, right)
 }
 
 func (m RdsViewModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	slog.Debug(fmt.Sprintf("%v", msg))
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	// TODO: This should be a custom WindowSizeMsg from Root
+	case tea.WindowSizeMsg:
+		slog.Debug(fmt.Sprintf("Received WindowSizeMsg %v", msg))
+		constants.WindowSize = msg
+
+		h, v := constants.DocStyle.GetFrameSize()
+
+		contentWidth := msg.Width - h
+		contentHeight := msg.Height - v
+
+		m.list.SetSize(contentWidth, contentHeight)
 	case rdsInstancesLoadedMsg:
 		m.isLoading = false
-		m.list = toTeaList(msg)
+		m.list.SetItems(msg)
 		slog.Debug(fmt.Sprintf("Size of list %d", len(m.list.Items())))
 	case spinner.TickMsg:
-		slog.Debug("Spinning")
 		m.loader, cmd = m.loader.Update(msg)
 	default:
 		m.list, cmd = m.list.Update(msg)
@@ -92,13 +128,40 @@ func dbInstancesToItems(dbs []aws.RDSInstance) []list.Item {
 	return items
 }
 
-func toTeaList(rdsInstances rdsInstancesLoadedMsg) list.Model {
-	h, v := constants.DocStyle.GetFrameSize()
-	//list.SetSize(constants.WindowSize.Width-h, constants.WindowSize.Height-v)
-	list := list.New(rdsInstances, list.NewDefaultDelegate(), constants.WindowSize.Width-h, constants.WindowSize.Height-v)
+func generateInstanceDetail(selectedItem list.Item) string {
+	// Skeleton detail sections
+	if selectedItem == nil {
+		return "No Info"
+	}
 
-	list.Title = "Instances"
-	list.SetShowStatusBar(false)
+	rds, ok := selectedItem.(aws.RDSInstance)
+	if !ok {
+		return "No Info"
+	}
 
-	return list
+	rows := []string{
+		"Instance Details",
+		"\n",
+		"General Info",
+		fmt.Sprintf("DB Identifier:    %s", rds.Id),
+		fmt.Sprintf("Engine:           %s", rds.DbEngine),
+		fmt.Sprintf("Version:          %s", rds.DbVersion),
+		fmt.Sprintf("Instance Class:   %s", rds.InstanceClass),
+		fmt.Sprintf("Status:           %s", rds.Status),
+		fmt.Sprintf("Total Storage:    %d", rds.AllocatedStorage),
+		fmt.Sprintf("Endpoint:         %s", rds.Endpoint),
+		fmt.Sprintf("Port:             %d", rds.Port),
+		fmt.Sprintf("IsPublic:         %t", rds.IsPubliclyAccessible),
+		"\n",
+		"Network",
+		fmt.Sprintf("VPC:			   %s", rds.VpcID),
+		fmt.Sprintf("Subnets:          %s", ""),
+		"\n",
+		"Metrics",
+		fmt.Sprintf("CPU Utilization:  %s", "Noop"),
+		fmt.Sprintf("Free Storage:     %s", "Noop"),
+		fmt.Sprintf("Connections:      %s", "Noop"),
+	}
+
+	return lipgloss.JoinVertical(lipgloss.Left, rows...)
 }
