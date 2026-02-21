2f125124dbd66636a2a98ffe6d8e4d22dd1580d6
Robert Benson <derelbenkoenig@gmail.com>
2026-02-20 19:43:11 -0500

    add test for s3 view logic

---
+++ internal/providers/aws/views/s3_test.go
@@ -0,0 +1,40 @@
+package views
+
+import (
+	aws "flying_nimbus/internal/providers/aws/backend"
+	"testing"
+)
+
+func TestGetSubtreeListItems(t *testing.T) {
+	fileTree := &aws.S3FileTree{
+		Files: []string{"file1", "file2"},
+		Subdirs: map[string]*aws.S3FileTree{
+			"dir1": &aws.S3FileTree{
+				Files: []string{},
+				Subdirs: map[string]*aws.S3FileTree{
+					"dir2": &aws.S3FileTree{
+						Files: []string{"file3", "file4"},
+						Subdirs: map[string]*aws.S3FileTree{
+							"dir3": &aws.S3FileTree{
+								Files:   []string{"file5", "file6"},
+								Subdirs: map[string]*aws.S3FileTree{},
+							},
+							"dir4": &aws.S3FileTree{
+								Files:   []string{"file7", "file8"},
+								Subdirs: map[string]*aws.S3FileTree{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	path := []string{"dir1", "dir2"}
+
+	listItems := getSubtreeListItems(path, fileTree)
+
+	if len(listItems) != 4 {
+		t.Errorf("wrong list length, should be 4 got %d", len(listItems))
+	}
+}
